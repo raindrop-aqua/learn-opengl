ca8cd5910d7aa474bd62dd7654033318801d2687
Masahiro Atsum <raindrop.aqua@gmail.com>
2014-06-14 14:28:07 +0900

    09番まで終了しました。

--- opngl_2d/tutorial_opengl09/main.cpp
+++ opngl_2d/tutorial_opengl09/main.cpp
@@ -16,9 +16,14 @@
 #if defined (_MSC_VER)
 #pragma comment(lib, "GLFWDLL.lib")
 #pragma comment(lib, "opengl32.lib")
+#ifdef _DEBUG
+#pragma comment(lib, "glewsd.lib")
+#else
+#pragma comment(lib, "glew32s.lib")
+#endif
 #endif
 
-bool setupTexture(const GLuint id, const char* file) {
+bool setupTexture(const GLuint id, const char* file, const int width, const int height) {
     std::ifstream fstr(file, std::ios::binary);
 
     if (!fstr) {
@@ -32,11 +37,14 @@ bool setupTexture(const GLuint id, const char* file) {
     fstr.read(&texture_buffer[0], file_size);
     
     glBindTexture(GL_TEXTURE_2D, id);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 256, 256, 0, GL_RGBA, GL_UNSIGNED_BYTE, &texture_buffer[0]);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-//    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-//    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, &texture_buffer[0]);
+//    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+//    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
     
     return true;
 }
@@ -54,34 +62,41 @@ int main() {
         glfwTerminate();
         return EXIT_FAILURE;
     }
+    glfwSwapInterval(1);
+
+#if defined (_MSC_VER)
+    if (glewInit() != GLEW_OK) {
+        glfwTerminate();
+        return EXIT_FAILURE;
+    }
+#endif
     
     GLuint texture_id;
     glGenTextures(1, &texture_id);
     
-    if (!setupTexture(texture_id, "sample.raw")) {
+    if (!setupTexture(texture_id, "sample.raw", 256, 256)) {
         glDeleteTextures(1, &texture_id);
         glfwTerminate();
         return EXIT_FAILURE;
     }
-    glfwSwapInterval(1);
     
     while (glfwGetWindowParam(GLFW_OPENED)) {
         glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
         glClear(GL_COLOR_BUFFER_BIT);
         
         static const GLfloat vtx[] = {
-            -0.5f, -0.5f,
-             0.5f, -0.5f,
-             0.5f,  0.5f,
-            -0.5f,  0.5f,
+            -0.8f, -0.8f,
+             0.8f, -0.8f,
+             0.8f,  0.8f,
+            -0.8f,  0.8f,
         };
         glVertexPointer(2, GL_FLOAT, 0, vtx);
 
         static const GLfloat texture_uv[] = {
-            0.2f, 0.5f,
-            0.8f, 0.5f,
-            0.8f, 0.0f,
-            0.2f, 0.0f,
+            -2.5f, 0.0f,
+             2.5f, 0.5f,
+             2.5f, 5.0f,
+            -2.5f, 5.0f,
         };
         glTexCoordPointer(2, GL_FLOAT, 0, texture_uv);
 
